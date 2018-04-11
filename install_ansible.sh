5a809a0607e8cd6750e5d06415cc2a98b2e89fa7
Dmitry <dmitry.vaghin@gmail.com>
2018-04-11 22:38:17 +0000

    update ansible role_path; display the settings

--- install_ansible.sh
+++ install_ansible.sh
@@ -5,3 +5,15 @@ sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
 ansible --version
+echo "== == =="
+echo "Ansible configuration fiel:"
+cat /etc/ansible/ansible.cfg | grep -v "#" | grep -v -e '^$'
+echo "== == =="
+echo "ANSIBLE_ROLES_PATH"
+echo ${ANSIBLE_ROLES_PATH}
+sed -i s/#roles_path/roles_path/ /etc/ansible/ansible.cfg
+echo "Ansible configuration fiel:"
+cat /etc/ansible/ansible.cfg | grep -v "#" | grep -v -e '^$'
+echo "== == =="
+echo "ANSIBLE_ROLES_PATH"
+echo ${ANSIBLE_ROLES_PATH}
