52a99515e29123c36406c7afc43fa3feb1fb44ee
Dmitry <dmitry.vaghin@gmail.com>
2018-04-24 22:01:40 +0000

    --tags key has been added

--- gcloud_create_instance.sh
+++ gcloud_create_instance.sh
@@ -2,5 +2,9 @@
 
 echo "Enter new instance name"
 read my_instance_name
-gcloud compute instances create ${my_instance_name} --machine-type g1-small --zone us-west1-c --image-family ubuntu-1604-lts --image-project ubuntu-os-cloud
+gcloud compute instances create ${my_instance_name} --machine-type g1-small --zone us-west1-c --image-family ubuntu-1604-lts --image-project ubuntu-os-cloud 
+
+# Add tags if necessary:
+# e.g. --tags=jenkins
+
 gcloud compute instances list
