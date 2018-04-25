5c53f97575d685f5d7523c6a9421bce279d0dd58
Dmitry <dmitry.vaghin@gmail.com>
2018-04-25 05:08:10 +0000

    new galaxy nginx role installed

--- install_nginx_proxy.sh
+++ install_nginx_proxy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #https://galaxy.ansible.com/geerlingguy/jenkins/
 
-sudo ansible-galaxy install openmicroscopy.nginx-proxy
+#sudo ansible-galaxy install openmicroscopy.nginx-proxy
+sudo ansible-galaxy install ypsman.nginx-proxy
 
 ansible-playbook -e "" ansible_playbooks/nginx-proxy.yml
