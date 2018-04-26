c41684aa12c3200115acd274a8133850f2dd76b8
Dmitry <dmitry.vaghin@gmail.com>
2018-04-26 22:11:06 +0000

    script to create self signed certificate

--- install_nginx_proxy.sh
+++ install_nginx_proxy.sh
@@ -4,4 +4,7 @@
 #sudo ansible-galaxy install openmicroscopy.nginx-proxy
 sudo ansible-galaxy install ypsman.nginx-proxy
 
+# Create self signed certificate
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+
 ansible-playbook -e "" ansible_playbooks/nginx-proxy.yml
